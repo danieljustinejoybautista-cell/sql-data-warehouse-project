12847261a2a8b3668a4ed7a6e1a054961209f317
danieljustinejoybautista-cell <danieljustinejoybautista@gmail.com>
2026-05-28 15:21:58 +0800

    Update init_table_datas.sql

--- Scripts/init_table_datas.sql
+++ Scripts/init_table_datas.sql
@@ -19,70 +19,85 @@ Warning:
   Running this script will remove all the data from the table above then replace with the datas in the file folder.
   Any changes will be removed and be replaced by the datas in the file folder.
 */
-truncate table bronze.crm_cust_info;
 
-bulk insert bronze.crm_cust_info
-from 'C:\DataWarehouse\source_crm\cust_info.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+create or alter procedure bronze.load_bronze as 
+begin
+;
+	print '================================================================'
+	print 'Loading Bronze Layer';
+	print '================================================================';
 
-truncate table bronze.crm_prd_info;
+	print ' ---------------------------------------------------------------';
+	print 'Loading CRM	Tables';
+	print ' ---------------------------------------------------------------';
 
-bulk insert bronze.crm_prd_info
-from 'C:\DataWarehouse\source_crm\prd_info.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+	truncate table bronze.crm_cust_info;
 
-truncate table bronze.crm_sales_details;
+	bulk insert bronze.crm_cust_info
+	from 'C:\DataWarehouse\source_crm\cust_info.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
 
-bulk insert bronze.crm_sales_details
-from 'C:\DataWarehouse\source_crm\sales_details.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+	truncate table bronze.crm_prd_info;
 
---
+	bulk insert bronze.crm_prd_info
+	from 'C:\DataWarehouse\source_crm\prd_info.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
 
-truncate table bronze.erp_cust_az12;
+	truncate table bronze.crm_sales_details;
 
-bulk insert bronze.erp_cust_az12
-from 'C:\DataWarehouse\source_erp\CUST_AZ12.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+	bulk insert bronze.crm_sales_details
+	from 'C:\DataWarehouse\source_crm\sales_details.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
 
-truncate table bronze.erp_loc_a101;
+	print ' ---------------------------------------------------------------';
+	print 'Loading ERP	Tables';
+	print ' ---------------------------------------------------------------';
 
-bulk insert bronze.erp_loc_a101
-from 'C:\DataWarehouse\source_erp\LOC_A101.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+	truncate table bronze.erp_cust_az12;
 
-truncate table bronze.erp_px_cat_g1v2;
+	bulk insert bronze.erp_cust_az12
+	from 'C:\DataWarehouse\source_erp\CUST_AZ12.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
 
-bulk insert bronze.erp_px_cat_g1v2
-from 'C:\DataWarehouse\source_erp\PX_CAT_G1V2.csv'
-with
-(
-	firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+	truncate table bronze.erp_loc_a101;
+
+	bulk insert bronze.erp_loc_a101
+	from 'C:\DataWarehouse\source_erp\LOC_A101.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	truncate table bronze.erp_px_cat_g1v2;
+
+	bulk insert bronze.erp_px_cat_g1v2
+	from 'C:\DataWarehouse\source_erp\PX_CAT_G1V2.csv'
+	with
+	(
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+end
