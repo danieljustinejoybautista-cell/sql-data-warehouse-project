50fafca5e8af52b6ae9f002e9b08af42b71520b7
danieljustinejoybautista-cell <danieljustinejoybautista@gmail.com>
2026-05-28 15:36:05 +0800

    Update init_table_datas.sql

--- Scripts/init_table_datas.sql
+++ Scripts/init_table_datas.sql
@@ -22,82 +22,104 @@ Warning:
 
 create or alter procedure bronze.load_bronze as 
 begin
-;
-	print '================================================================'
-	print 'Loading Bronze Layer';
-	print '================================================================';
+	declare @start_time datetime, @end_time datetime;
+	begin try
+		print '================================================================';
+		print 'Loading Bronze Layer';
+		print '================================================================';
 
-	print ' ---------------------------------------------------------------';
-	print 'Loading CRM	Tables';
-	print ' ---------------------------------------------------------------';
+		print ' ---------------------------------------------------------------';
+		print 'Loading CRM	Tables';
+		print ' ---------------------------------------------------------------';
 
-	truncate table bronze.crm_cust_info;
+		set @start_time = GETDATE();
+		truncate table bronze.crm_cust_info;
 
-	bulk insert bronze.crm_cust_info
-	from 'C:\DataWarehouse\source_crm\cust_info.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		bulk insert bronze.crm_cust_info
+		from 'C:\DataWarehouse\source_crm\cust_info.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
 
-	truncate table bronze.crm_prd_info;
+		truncate table bronze.crm_prd_info;
 
-	bulk insert bronze.crm_prd_info
-	from 'C:\DataWarehouse\source_crm\prd_info.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		bulk insert bronze.crm_prd_info
+		from 'C:\DataWarehouse\source_crm\prd_info.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
 
-	truncate table bronze.crm_sales_details;
+		truncate table bronze.crm_sales_details;
 
-	bulk insert bronze.crm_sales_details
-	from 'C:\DataWarehouse\source_crm\sales_details.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		bulk insert bronze.crm_sales_details
+		from 'C:\DataWarehouse\source_crm\sales_details.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
 
-	print ' ---------------------------------------------------------------';
-	print 'Loading ERP	Tables';
-	print ' ---------------------------------------------------------------';
+		set @end_time = GETDATE();
+		print '>> Load Duration:' + cast(datediff(second, @start_time, @end_time) as nvarchar) + ' seconds'
+		print '>> -------------------------------------------------------------';
 
-	truncate table bronze.erp_cust_az12;
+		print ' ---------------------------------------------------------------';
+		print 'Loading ERP	Tables';
+		print ' ---------------------------------------------------------------';
 
-	bulk insert bronze.erp_cust_az12
-	from 'C:\DataWarehouse\source_erp\CUST_AZ12.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		set @start_time = GETDATE();
 
-	truncate table bronze.erp_loc_a101;
+		truncate table bronze.erp_cust_az12;
 
-	bulk insert bronze.erp_loc_a101
-	from 'C:\DataWarehouse\source_erp\LOC_A101.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		bulk insert bronze.erp_cust_az12
+		from 'C:\DataWarehouse\source_erp\CUST_AZ12.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
 
-	truncate table bronze.erp_px_cat_g1v2;
+		truncate table bronze.erp_loc_a101;
 
-	bulk insert bronze.erp_px_cat_g1v2
-	from 'C:\DataWarehouse\source_erp\PX_CAT_G1V2.csv'
-	with
-	(
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	);
+		bulk insert bronze.erp_loc_a101
+		from 'C:\DataWarehouse\source_erp\LOC_A101.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+
+		truncate table bronze.erp_px_cat_g1v2;
+
+		bulk insert bronze.erp_px_cat_g1v2
+		from 'C:\DataWarehouse\source_erp\PX_CAT_G1V2.csv'
+		with
+		(
+			firstrow = 2,
+			fieldterminator = ',',
+			tablock
+		);
+
+		set @end_time = GETDATE();
+		print '>> Load Duration:' + cast(datediff(second, @start_time, @end_time) as nvarchar) + ' seconds'
+		print '>> -------------------------------------------------------------';
+
+	end try
+	begin catch
+		print '=======================================================================';
+		print 'Error Occur During Loading Bronze layer';
+		print 'Error Message' + error_message();
+		print 'Error Message' + cast (error_number() as nvarchar);
+		print 'Error Message' + cast (error_state() as nvarchar);
+		print '=======================================================================';
+	end catch
 end
